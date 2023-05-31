5e7490ef4866ffddac804804f9d9c11084b5fe7d
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-06-01 00:24:44 +0430

    [ theme ] mui

---
+++ src/theme/defualtTheme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#007bff',
+    },
+    secondary: {
+      main: '#edf2ff',
+    },
+  },
+});
+
+export default theme;
