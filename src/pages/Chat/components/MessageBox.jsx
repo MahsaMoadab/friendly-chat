1901b3e3d3383a9cbaf9c4b8261b22b1947ad687
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-06-04 13:00:58 +0430

    habdle not show img

--- src/pages/Chat/components/MessageBox.jsx
+++ src/pages/Chat/components/MessageBox.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import Moment from 'react-moment';
-
+import Default from "../../../assets/images/image.svg";
 
 export const MessageBox = ({ message, user }) => {
 
@@ -8,13 +8,18 @@ export const MessageBox = ({ message, user }) => {
 
   useEffect(() => {
     scrollRef.current?.scrollIntoView({ behavior: "smooth" })
-  }, [message])
+  }, [message]);
+  
+  const onImageError = (e) => {
+    e.target.src = Default;
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" })
+  }
 
   return (
     <div className='message__content' ref={scrollRef}>
       <div className={`message_box ${message.from === user.uid ? 'you' : ''}`}>
         {
-          message.media && <img src={message.media} alt="" />
+          message.media && <img src={message.media} alt='' onError={onImageError} />
         }
         {
           message.text && <p>{message.text}</p>
