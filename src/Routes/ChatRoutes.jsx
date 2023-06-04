b2bec7cf5d44eb9df47dffe727f4489f69356178
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-06-04 13:01:13 +0430

    [ Auth ] [ protected ]

--- src/Routes/ChatRoutes.jsx
+++ src/Routes/ChatRoutes.jsx
@@ -5,6 +5,7 @@ import Chat from '../pages/Chat/Chat'
 import SignIn from '../pages/Auth/SignIn/SignIn'
 import SignUp from '../pages/Auth/SignUp/SignUp'
 import { UserAuth } from '../services/auth/authContext'
+import AuthProtected from './AuthProtected'
 
 export default function ChatRoutes({ theme }) {
   const { user } = UserAuth();
@@ -12,8 +13,8 @@ export default function ChatRoutes({ theme }) {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ProtectedRoute user={user}><Chat /></ProtectedRoute>} />
-        <Route path="auth/sign_in" element={<SignIn />} />
-        <Route path="auth/sign_up" element={<SignUp />} />
+        <Route path="auth/sign_in" element={<AuthProtected user={user}><SignIn /></AuthProtected>} />
+        <Route path="auth/sign_up" element={<AuthProtected user={user}><SignUp /></AuthProtected>} />
       </Routes>
     </BrowserRouter>
   )
