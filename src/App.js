0472168f16f4f1e579d0b30c7d6ef2070ea536b5
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-06-04 12:54:34 +0430

    import eva-icons.css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './assets/style/index.scss';
 import { AuthContextProvider } from './services/auth/authContext';
 import ChatRoutes from './Routes';
 import useDarkMode from './theme/useDarkMode';
+import 'eva-icons/style/eva-icons.css';
 
 function App() {
   const [theme] = useDarkMode();
