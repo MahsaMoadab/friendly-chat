6889216159fedc8171f53ad585f6c63d923930d2
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-06-05 23:17:28 +0430

    text call message

--- src/pages/Chat/components/MessageBox.jsx
+++ src/pages/Chat/components/MessageBox.jsx
@@ -2,32 +2,36 @@ import React, { useEffect, useRef } from 'react'
 import Moment from 'react-moment';
 import Default from "../../../assets/images/image.svg";
 
-export const MessageBox = ({ message, user }) => {
+export const MessageBox = ({ message, user, handleJoinTiCall }) => {
 
   const scrollRef = useRef();
 
   useEffect(() => {
     scrollRef.current?.scrollIntoView({ behavior: "smooth" })
   }, [message]);
-  
+
   const onImageError = (e) => {
     e.target.src = Default;
     scrollRef.current?.scrollIntoView({ behavior: "smooth" })
   }
 
   return (
-    <div className='message__content' ref={scrollRef}>
-      <div className={`message_box ${message.from === user.uid ? 'you' : ''}`}>
-        {
-          message.media && <img src={message.media} alt='' onError={onImageError} />
-        }
-        {
-          message.text && <p>{message.text}</p>
-        }
+    <div className='message__content'>
+      <div ref={scrollRef}>
+        <div className={`message_box ${message.from === user.uid ? 'you' : ''}`}>
+          {
+            message.media && <img src={message.media} alt='' onError={onImageError} />
+          }
+          {
+            message.text && <p>{message.text}</p>
+          }
+          {message.call && <p>Video message...</p>}
+        </div>
+        <small className={`time ${message.from === user.uid ? 'you' : ''}`}>
+          <Moment date={message.createdAt.toDate()} format="LT" />
+        </small>
       </div>
-      <small className={`time ${message.from === user.uid ? 'you' : ''}`}>
-        <Moment date={message.createdAt.toDate()} format="LT" />
-      </small>
     </div>
+
   )
 }
