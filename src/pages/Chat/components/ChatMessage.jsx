a4291a9eb71bede1895667f122919a88cbc8bf51
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-06-05 23:15:25 +0430

    handle call

--- src/pages/Chat/components/ChatMessage.jsx
+++ src/pages/Chat/components/ChatMessage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import SetNameProfile from './SetNameProfile'
-import { Button, Divider, Grow } from '@mui/material'
+import { Button, Divider } from '@mui/material'
 import { useEffect } from 'react';
 import SendIcon from '../../../assets/images/send.svg'
 import { Timestamp, addDoc, collection, doc, updateDoc } from 'firebase/firestore';
@@ -11,31 +11,43 @@ import { MessageBox } from './MessageBox';
 import Moment from 'react-moment';
 import Attach from "../../../assets/images/icons/attach.svg";
 import BackIcon from "../../../assets/images/icons/back.svg";
+import VideoIcon from "../../../assets/images/icons/video.svg";
+import Call from './Call';
+import { useRef } from 'react';
 
 const ChatMessage = ({ chat, messageList, closeChat }) => {
 
-    const [animation, setAnimation] = useState(false);
+    // const [animation, setAnimation] = useState(false);
     const { user } = UserAuth();
     const [text, setText] = useState('');
     const [img, setImg] = useState();
     const user1 = user.uid;
     const user2 = chat.uid;
     const messageId = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
-    console.log(user);
+    const [call, setCall] = useState(false);
+    let lastCall = "";
+    const scrollRef = useRef();
+
+
     useEffect(() => {
-        setAnimation(true);
-    }, [chat]);
+        setCall(false);
+        lastCall = messageList.at(-1);
+        scrollRef.current?.scrollIntoView({ behavior: "smooth" })
+    }, [chat, messageList]);
 
     const handleAttach = (file) => {
         setImg(file);
         const fileType = file['type'];
-        const validImageTypes = ['image/gif', 'image/jpeg', 'image/png', 'image/svgz', 'image/ico', 'image/tif'];
+        const validImageTypes = ['image/gif', 'image/jpeg', 'image/svg+xml', 'image/png', 'image/svgz', 'image/ico', 'image/tif'];
         if (!validImageTypes.includes(fileType)) {
             console.log("no image");
         }
     }
     const sendMessageHandle = async (e) => {
         e.preventDefault();
+        console.log(img);
+        console.log(text);
+        if (!text) return;
         let url = '';
         if (img) {
             const imgRef = ref(userStorage, `images/${new Date().getTime()}-${img.name}`);
@@ -48,6 +60,7 @@ const ChatMessage = ({ chat, messageList, closeChat }) => {
             from: user1,
             to: user2,
             media: url || '',
+            call: null,
             createdAt: Timestamp.fromDate(new Date()),
         });
 
@@ -62,7 +75,8 @@ const ChatMessage = ({ chat, messageList, closeChat }) => {
                 uid: chat.uid,
                 avatar: chat.photoURL ? chat.photoURL : null,
                 text: messageText || '',
-                media: url || '',
+                media: img ? img.name : '',
+                call: null,
             },
         });
 
@@ -74,19 +88,64 @@ const ChatMessage = ({ chat, messageList, closeChat }) => {
                 uid: user.uid,
                 avatar: user.photoURL ? user.photoURL : null,
                 text: messageText || '',
-                media: url || '',
+                media: img ? img.name : '',
+                call: null,
             },
         });
+        setImg();
     }
 
+    const handleVideoCall = async (e) => {
+        e.preventDefault();
+        await addDoc(collection(userDB, "messages", messageId, "chat"), {
+            text: '',
+            from: user1,
+            to: user2,
+            media: '',
+            call: "video",
+            createdAt: Timestamp.fromDate(new Date()),
+        });
+        await updateDoc(doc(userDB, "userChats", user.uid), {
+            [messageId + ".date"]: Timestamp.fromDate(new Date()),
+            [messageId + ".unread"]: false,
+            [messageId + ".userInfo"]: {
+                name: chat.name,
+                uid: chat.uid,
+                avatar: chat.photoURL ? chat.photoURL : null,
+                text: '',
+                media: '',
+                call: "video",
+            },
+        });
+
+        await updateDoc(doc(userDB, "userChats", chat.uid), {
+            [messageId + ".date"]: Timestamp.fromDate(new Date()),
+            [messageId + ".unread"]: true,
+            [messageId + ".userInfo"]: {
+                name: user.displayName,
+                uid: user.uid,
+                avatar: user.photoURL ? user.photoURL : null,
+                text: '',
+                media: '',
+                call: "video",
+            },
+        });
+        setCall(true);
+    }
+
+    const handleJoinTiCall = () => {
+        console.log("calll");
+        setCall(true);
+    }
+    const handleCloseCall = () => {
+        console.log("calll ended");
+        lastCall = "";
+        setCall(false);
+    }
     return (
-        <Grow
-            in={animation}
-            style={{ transformOrigin: '0 0 0' }}
-            {...(animation ? { timeout: 800 } : {})}
-        >
+        <>
             <div className='chat_container'>
-                <div className="message">
+                <div className={`message ${call ? 'hidden' : ''}`}>
                     <div className="message_header">
                         <div className="message_header_user">
                             <div className='user'>
@@ -100,6 +159,9 @@ const ChatMessage = ({ chat, messageList, closeChat }) => {
                             </div>
                         </div>
                         <div className='message_header_action'>
+                            <button className='call_btn' onClick={handleVideoCall}>
+                                <img src={VideoIcon} alt="" />
+                            </button>
                             <button className='close__Chat' onClick={closeChat}><img src={BackIcon} alt="" /></button>
                         </div>
                     </div>
@@ -108,13 +170,17 @@ const ChatMessage = ({ chat, messageList, closeChat }) => {
                         {
                             (messageList.length > 0) ?
                                 messageList.map((msg, index) => {
-                                    return <MessageBox key={index} message={msg} user={user} />
+                                    return <MessageBox key={index} message={msg} user={user} handleJoinTiCall={handleJoinTiCall} />
                                 }) :
                                 <div className='no_message'>
                                     <p>
                                         No messages here yet...
                                     </p>
                                 </div>
+                        }
+                        
+                        {
+                            lastCall && lastCall.call === 'video' ? <div className='message__content'><div ref={scrollRef} className={`message_box call ${lastCall.from === user.uid ? 'you' : ''}`}><button onClick={handleJoinTiCall}>Join to Call</button></div></div>  : ''
                         }
                     </div>
                     <Divider />
@@ -135,8 +201,12 @@ const ChatMessage = ({ chat, messageList, closeChat }) => {
                         </div>
                     </form>
                 </div>
+                {call && 
+                <div className={`view_call join_call`}>
+                    <Call roomName={messageId} username={user.displayName} handleCloseCall={handleCloseCall}/>
+                </div>}
             </div>
-        </Grow>
+        </>
     )
 }
 
