4f5712c2381b0fad6409d2351c5dfd78eb8d543e
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-06-09 17:11:59 +0430

    [ useTranslation ]

--- src/pages/Chat/components/MessageBox.jsx
+++ src/pages/Chat/components/MessageBox.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useRef } from 'react'
 import Moment from 'react-moment';
 import Default from "../../../assets/images/image.svg";
+import { useTranslation } from 'react-i18next';
 
 export const MessageBox = ({ message, user, handleJoinTiCall }) => {
 
   const scrollRef = useRef();
+  const { t } = useTranslation();
 
   useEffect(() => {
     scrollRef.current?.scrollIntoView({ behavior: "smooth" })
@@ -12,25 +14,31 @@ export const MessageBox = ({ message, user, handleJoinTiCall }) => {
 
   const onImageError = (e) => {
     e.target.src = Default;
-    scrollRef.current?.scrollIntoView({ behavior: "smooth" })
+    // scrollRef.current?.scrollIntoView({ behavior: "smooth" })
   }
 
   return (
-    <div className='message__content'>
-      <div ref={scrollRef}>
-        <div className={`message_box ${message.from === user.uid ? 'you' : ''}`}>
-          {
-            message.media && <img src={message.media} alt='' onError={onImageError} />
-          }
-          {
-            message.text && <p>{message.text}</p>
-          }
-          {message.call && <p>Video message...</p>}
-        </div>
-        <small className={`time ${message.from === user.uid ? 'you' : ''}`}>
-          <Moment date={message.createdAt.toDate()} format="LT" />
-        </small>
-      </div>
+    <div ref={scrollRef}>
+      {
+        message.call !== "endCall" ?
+          <div className='message__content'>
+            <div>
+              <div className={`message_box ${message.from === user.uid ? 'you' : ''}`}>
+                {
+                  message.media && <img src={message.media} alt='' onError={onImageError} />
+                }
+                {
+                  message.text && <p>{message.text}</p>
+                }
+                {message.call && <p>{t('Video call')}...</p>}
+              </div>
+              <small className={`time ${message.from === user.uid ? 'you' : ''}`}>
+                <Moment date={message.createdAt.toDate()} format="LT" />
+              </small>
+            </div>
+          </div>
+          : null
+      }
     </div>
 
   )
