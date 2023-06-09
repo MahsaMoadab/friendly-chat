041fbc6b3083416f5b17aa1902be9cf49d0cb684
MahsaMoadab <mahsa.moadab.s@gmail.com>
2023-06-09 17:07:18 +0430

    set lang & enable dark mode toastContainer

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import { AuthContextProvider } from './services/auth/authContext';
 import ChatRoutes from './Routes';
 import useDarkMode from './theme/useDarkMode';
 import 'eva-icons/style/eva-icons.css';
+import useLang from './lang/useLang';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [theme] = useDarkMode();
-
+  const [lang] = useLang();
+  
   return (
     <AuthContextProvider>
       <ChatRoutes />
+      <ToastContainer theme={theme} rtl={lang === "fa" ? true : false}/>
     </AuthContextProvider>
   );
 }
